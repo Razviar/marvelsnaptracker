16c9a30203d4dacb9fbfbfce562619249c28ce45
Rual Ilmarranen <ilmarranen@gmail.com>
2022-11-12 19:45:48 +0200

    not needed stuff removed

--- src/models/metadata.ts
+++ src/models/metadata.ts
@@ -1,31 +1,3 @@
-import {Card} from 'root/models/cards';
-
-export interface Metadata {
-  exps: {[index: number]: {code: string; currentstandard: number; name: string}};
-  expsorder: number[];
-  formats: {
-    ClosedTime: number;
-    DeckSelect: string;
-    EntryFees: string;
-    LockedTime: number;
-    MaxLosses: number;
-    MaxWins: number;
-    PublicEventName: string;
-    StartTime: number;
-    date: number;
-    format: string;
-    name: string;
-  }[];
-  datefilters: number[];
-  datefilterlabels: string[];
-  types: {id: number; is_creature: number; name: string; parent_id: number}[];
-  archetypes: {id: number; name: string; supertype: number}[];
-  allcards: [number, Card][];
-  mtgatoinnerid: {[index: number]: number};
-  trackerver: {version: number; date: number};
-  headerbanners: string[];
-}
-
 export interface UserMetadata {
   user: Userdata;
   collection: {[index: number]: {ic: number; ir: number; it: number}};
--- src/windows/home/functions/messages.ts
+++ src/windows/home/functions/messages.ts
@@ -53,56 +53,6 @@ export function installHomeMessages(): void {
     }
   });
 
-  onMessageFromIpcMain('set-o-settings', (newOSettings) => {
-    const overlaySettingsBoolean: Message[] = [
-      'set-setting-o-hidezero',
-      'set-setting-o-showcardicon',
-      'set-setting-o-hidemy',
-      'set-setting-o-hideopp',
-      'set-setting-o-timers',
-      'set-setting-o-neverhide',
-      'set-setting-o-mydecks',
-      'set-setting-o-cardhover',
-      'set-setting-o-detach',
-      'set-setting-o-hidemain',
-      'set-setting-o-interactive',
-    ];
-    const overlaySettingsNumber: Message[] = [
-      'set-setting-o-leftdigit',
-      'set-setting-o-rightdigit',
-      'set-setting-o-leftdraftdigit',
-      'set-setting-o-rightdraftdigit',
-      'set-setting-o-bottomdigit',
-      'set-setting-o-fontcolor',
-    ];
-
-    overlaySettingsBoolean.forEach((settingName) => {
-      const settingType = settingName.split('set-setting-o-')[1] ?? '';
-      const sw = document.querySelector(`[data-setting="o-${settingType}"]`) as HTMLInputElement;
-      if (hasOwnProperty(newOSettings, settingType)) {
-        const value = newOSettings[settingType];
-        if (typeof value === 'boolean') {
-          sw.checked = value;
-        }
-      }
-    });
-
-    overlaySettingsNumber.forEach((settingName) => {
-      const settingType = settingName.split('set-setting-o-')[1] ?? '';
-      const sw = document.querySelector(`[data-setting="o-${settingType}"]`) as HTMLSelectElement;
-      const opts = sw.options;
-      sw.selectedIndex = Array.from(opts).findIndex((opt) => {
-        if (hasOwnProperty(newOSettings, settingType)) {
-          const value = newOSettings[settingType];
-          if (typeof value === 'number') {
-            return +opt.value === value;
-          }
-        }
-        return false;
-      });
-    });
-  });
-
   onMessageFromIpcMain('set-settings', (newSettings) => {
     let output = `<div class="table"><div class='row'>
           <div class='cell header white'><strong>Nick</strong></div>
